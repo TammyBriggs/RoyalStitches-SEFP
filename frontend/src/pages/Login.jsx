428747854b834c258df985b15e025f4809adbd8f
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-11-22 22:04:45 +0100

    working on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Login = () => {
+
+  const [currentState,setCurrentState] = useState('Sign Up')
+
   return (
-    <div>
-        
-    </div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='cormorant-garamond-regular text-3xl'>{currentState}</p>
+      </div>
+    </form>
   )
 }
 
