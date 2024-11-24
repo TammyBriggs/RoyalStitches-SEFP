80a82a2e39913a1399d70c1a6c59d63f92722ef5
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-11-24 14:21:53 +0100

    logic for adding products

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,7 +22,7 @@ const loginUser = async (req,res) => {
         const isMatch = await bcrypt.compare(password,user.password);
 
         if (isMatch) {
-            const tokenn = createToken(user._id)
+            const token = createToken(user._id)
             res.json({success:true,token})
         }
         else{
