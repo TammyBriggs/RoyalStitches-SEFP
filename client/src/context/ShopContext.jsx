bd6f69e46134992c684d07f8f869166eb82f5e2a
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-11-26 23:04:03 +0100

    working on login and logout functionality on main site

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -13,6 +13,7 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
   const [products, setProducts] = useState([]);
+  const [token,setToken] = useState('');
   const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
@@ -91,6 +92,12 @@ const ShopContextProvider = (props) => {
     getProductsData();
   }, []);
 
+  useEffect(()=>{
+    if (!token && localStorage.getItem('token')) {
+      setToken(localStorage.getItem('token'))
+    }
+  },[])
+
   const value = {
     products,
     currency,
@@ -106,6 +113,8 @@ const ShopContextProvider = (props) => {
     getCartAmount,
     navigate,
     backendUrl,
+    setToken,
+    token,
   };
 
   return (
