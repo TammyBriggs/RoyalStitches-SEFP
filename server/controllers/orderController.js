472676b21c917279409e0cff3177bfffc5b1d3e3
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-11-27 16:02:59 +0100

    Order Model created

---
+++ server/controllers/orderController.js
@@ -0,0 +1,11 @@
+
+
+// Placing orders using COD Method
+const placeOrder = async (req,res) => {
+    
+}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req,res) => {
+    
+}
