cb07d611e1f02332295434a4bab9aa82a5afcfa5
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-11-28 13:13:19 +0100

    Flutterwave package added and back end functionalities for payment function added

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,14 @@
+import { currency } from "../../admin/src/App.jsx"
 import OrderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Flutterwave from 'flutterwave-react-v3'
+
+// Global variables
+const currency = 'usd'
+const deliveryCharge = 10
+
+// Gateway Initialize
+const flutterwave = new Flutterwave(process.env.FLUTTERWAVE_SECRET_KEY)
 
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
@@ -28,8 +37,62 @@ const placeOrder = async (req, res) => {
   }
 };
 
-// Placing orders using Stripe Method
-const placeOrderStripe = async (req, res) => {};
+// Placing orders using Flutterwave Method
+const placeOrderStripe = async (req, res) => {
+  try {
+    
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new OrderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+    price_data: {
+      currency: currency,
+      product_data: {
+        name:item.name 
+      },
+      unit_amount: item.price * 100
+    },
+    quantity: item.quantity
+    }))
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name:'Delivery Charges' 
+        },
+        unit_amount: deliveryCharge * 100
+      },
+      quantity: 1
+    })
+
+    const session = await flutterwave.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: 'payment',
+    })
+
+    res.json({success:true,session_url:session.url})
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
